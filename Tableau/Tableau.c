186bf5c50b3a7c1eb3d1fd0761467c4c8c57f7e9
Veritas <aanga26@gmail.com>
2021-06-02 20:28:38 +0300

    User input implementation complete

--- Tableau/Tableau.c
+++ Tableau/Tableau.c
@@ -58,12 +58,52 @@ void readTableau(Tableau *tab, const char * filename) {
 }
 
 void userInputTableau(Tableau *tab) {
-  
+  double a1, a2, a3;
+        printf("Enter the coefficients:\n");
+        printf("c1: ");
+        scanf("%lf", &a1);
+        tab->mat[0][1] = -a1;
+        printf("c2: ");
+        scanf("%lf", &a2);
+        tab->mat[0][2] = -a2;
+        printf("c3: ");
+        scanf("%lf", &a3);
+        tab->mat[0][3] = -a3;
+        printf("rhs: ");
+        scanf("%lf", &tab->mat[0][0]);
+        printf("Enter the constraint coefficients 1:\n");
+        printf("a11: ");
+        scanf("%lf", &tab->mat[1][1]);
+        printf("a12: ");
+        scanf("%lf", &tab->mat[1][2]);
+        printf("a13: ");
+        scanf("%lf", &tab->mat[1][3]);
+        printf("rhs: ");
+        scanf("%lf", &tab->mat[1][0]);
+        printf("Enter the constraint coefficients 2:\n");
+        printf("a21: ");
+        scanf("%lf", &tab->mat[2][1]);
+        printf("a22: ");
+        scanf("%lf", &tab->mat[2][2]);
+        printf("a23: ");
+        scanf("%lf", &tab->mat[2][3]);
+        printf("rhs: ");
+        scanf("%lf", &tab->mat[2][0]);
+        printf("Enter the constraint coefficients 3:\n");
+        printf("a31: ");
+        scanf("%lf", &tab->mat[3][1]);
+        printf("a31: ");
+        scanf("%lf", &tab->mat[3][2]);
+        printf("a31: ");
+        scanf("%lf", &tab->mat[3][3]);
+        printf("rhs: ");
+        scanf("%lf", &tab->mat[3][0]);
 }
 
 void print_optimal_vector(Tableau *tab, char *message) {
   int j, xi;
-  printf("%s at ", message);
+  printf("%s: %3.2lf at ", message, tab->mat[0][0]);
+
   for(j=1;j<tab->n;j++) { // for each column.
     xi = find_basis_variable(tab, j);
     if (xi != -1)
--- main.c
+++ main.c
@@ -1,41 +1,6 @@
 #include <stdio.h>
 #include "definitions/defs.h"
 
-    /*
-  What: Simplex in C
-  AUTHOR: GPL(C) moshahmed/at/gmail.
-
-  What: Solves LP Problem with Simplex:
-    { maximize cx : Ax <= b, x >= 0 }.
-  Input: { m, n, Mat[m x n] }, where:
-    b = mat[1..m,0] .. column 0 is b >= 0, so x=0 is a basic feasible solution.
-    c = mat[0,1..n] .. row 0 is z to maximize, note c is negated in input.
-    A = mat[1..m,1..n] .. constraints.
-    x = [x1..xm] are the named variables in the problem.
-    Slack variables are in columns [m+1..m+n]
-
-  USAGE:
-    1. Problem can be specified before main function in source code:
-      c:\> vim mosplex.c  
-      Tableau tab  = { m, n, {   // tableau size, row x columns.
-          {  0 , -c1, -c2,  },  // Max: z = c1 x1 + c2 x2,
-          { b1 , a11, a12,  },  //  b1 >= a11 x1 + a12 x2
-          { b2 , a21, a22,  },  //  b2 >= a21 x1 + a22 x2
-        }
-      };
-      c:\> cl /W4 mosplex.c  ... compile this file.
-      c:\> mosplex.exe problem.txt > solution.txt
-
-    2. OR Read the problem data from a file:
-      $ cat problem.txt
-            m n
-            0  -c1 -c2
-            b1 a11 a12
-            b2 a21 a11 
-      $ gcc -Wall -g mosplex.c  -o mosplex
-      $ mosplex problem.txt > solution.txt
-*/
-
 Tableau tab  = { 4, 4, {                     // Size of tableau [4 rows x 5 columns ]
     {  0.0 , -0.5 , -3.0 ,-1.0 ,   },  // Max: z = 0.5*x + 3*y + z,
     { 40.0 ,  1.0 ,  1.0 , 1.0 ,   },  //    x + y + z <= 40 .. b1
@@ -48,9 +13,7 @@ int main(int argc, char *argv[]){
     if (argc > 1) { // usage: cmd datafile
         readTableau(&tab, argv[1]);
     } else {
-        int a1, a2, a3, b1, b2, b3, c1, c2, c3, d1, d2, d3;
-        int rhs1, rhs2, rhs3;
-        printf("Enter the coefficients")
+        userInputTableau(&tab);
     }
     printTableau(&tab,"Initial");
     simplex(&tab);
